bf34ce7242999015374749d4b7568968944c935c
Maxwel-hyuga <maxwelhyuga@gmail.com>
2021-10-18 14:48:24 -0300

    segundo programa

---
+++ ex002.py
@@ -0,0 +1,5 @@
+##Este programa captura uma entrada e exibe na tela com uma mensagem de boas vindas!
+#!/usr/bin/python
+nome = input('Digite seu nome: ')
+print('É um prazer ti conhecer. {}!'.format(nome))
+
