11d3d01526725d6d641f3c7160fdb64992d4d03e
Maxwel-hyuga <maxwelhyuga@gmail.com>
2021-10-19 14:39:02 -0300

    terceiro programa

---
+++ ex003.py
@@ -0,0 +1,7 @@
+##Programa mostra a soma entre dois números!
+#!/usr/bin/python
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+print('A soma entre {} e {} é igual a {}!'.format(n1, n2, n1+n2))
+
+
