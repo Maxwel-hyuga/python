2854dd6f69f4698b8cf02ee187067674229be2f3
Maxwel-hyuga <maxwelhyuga@gmail.com>
2021-10-20 10:56:15 -0300

    sexto programa

---
+++ ex006.py
@@ -0,0 +1,5 @@
+##Programa exibe o dobro o triplo e a raiz quadrada de uma número!
+
+n = int(input('Digite um número: '))
+print('O número digitado foi {}; Seu dobro é {}; Seu triplo {}; E a raiz é {:.2f}'.format(n, n * 2, n * 3, pow(n,0.5)))
+
