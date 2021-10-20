7e40387be3935f8d7702df82fbcafc10ffc9ca1b
Maxwel-hyuga <maxwelhyuga@gmail.com>
2021-10-20 10:56:38 -0300

    sétimo programa

---
+++ ex007.py
@@ -0,0 +1,7 @@
+##Programa calcula á média aritimética entre dois números!
+
+n1 = float(input('Digite a primeira nota do aluno: '))
+n2 = float(input('Digite a segunda nota do aluno: '))
+media = (n1 + n2) / 2
+print('A primeira nota do aluno foi {}, e a segunda foi {}; Portanto a média alcançada foi {}'.format(n1, n2, media))
+ 
