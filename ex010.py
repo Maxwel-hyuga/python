e58a204c18b947996aba58c77f87f08ca7f3d0df
Maxwel-hyuga <maxwelhyuga@gmail.com>
2021-10-20 10:58:00 -0300

    décimo programa

---
+++ ex010.py
@@ -0,0 +1,8 @@
+##Programa para converter real para dolar!
+
+
+din = float(input('Quanto dinheiro você tem na carterira: R$'))
+dolar = 5.20
+result = din / dolar
+print('Eu tenho na carteria R$ {:.2f}, e isso convertido é equivalente U$${:.2f}'.format(din, result))
+ 
