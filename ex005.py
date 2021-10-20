5908665e15b4c5cd0050c23afe5b3917850d1c3b
Maxwel-hyuga <maxwelhyuga@gmail.com>
2021-10-20 10:55:38 -0300

    quinto programa

---
+++ ex005.py
@@ -0,0 +1,6 @@
+##Programa exibe sucessor e antecessor de um número!
+
+num = int(input('Digite um numero qualquer: '))
+print('Analisando o número {}, seu antecessor é {} e seu sucessor é {}'.format(num, num -1, num + 1))
+
+
