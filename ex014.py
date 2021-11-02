3548aa12ae2a50faba0b3110464fefb93c4ccfa2
Maxwel-hyuga <maxwelhyuga@gmail.com>
2021-11-01 21:01:28 -0300

    décimo quarto programa

---
+++ ex014.py
@@ -0,0 +1,6 @@
+##Programa exiber conversão de temperatura!
+
+c = float(input('Informe a temperatura em ºC: '))
+f = c * 9 / 5 + 32
+print('A temperatura de {}ºC corresponde a {:.2f}ºF!'.format(c, f))
+
