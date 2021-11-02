dbcafbf2eb76aac1ceb33db62bc9f5b3b04bc105
Maxwel-hyuga <maxwelhyuga@gmail.com>
2021-11-01 21:02:53 -0300

    décimo sexto programa

---
+++ ex016.py
@@ -0,0 +1,5 @@
+##Programa exiber a parte inteira de um número!
+from math import trunc
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua parte inteira é {}.'.format(num, trunc(num)))
+
